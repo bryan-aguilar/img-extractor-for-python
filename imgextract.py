fb5b423cb778face90b24ce07c622a45a57b4829
Bryan Aguilar <b.aguilar4@gmail.com>
2020-02-06 10:44:51 -0800

    Add user ability to define DL #

--- imgextract.py
+++ imgextract.py
@@ -12,25 +12,44 @@ reddit = praw.Reddit(client_id='oFIVlnHQfgXjYQ',
 def main():
     #TODO:
         #check for duplicates
-        #allow user to define their own subreddit
         #allow user to define their own search criteria(top/hot/recent/controversial)
         #allow user to define how many they would like to retrieve
 
     #stores a list of urls to a list of 10 hottest submissions
     
     user_sub = GetSub()
-    for submission in user_sub.top(limit=10):
+    num_download = GetNumDownload()
+
+    download_count = 0 
+    for submission in user_sub.top(limit=999):
+        if download_count == num_download:
+            break
         if CheckForNonImage(submission):
-            DownloadImage(submission)
+            download_count += DownloadImage(submission)
+def GetNumDownload():
+    #returns the amount of of images user would like to download
+    #currently softcapped at 100 until further error checking can be done
+    while 1:
+        try:
+            requested_amount = int(input("How many images would you like to download (Min: 1 Max:100): "))
+        except ValueError:
+            print("Please enter a valid number")
+            continue
+        if requested_amount < 1 or requested_amount > 100:
+            print("Please enter a valid number")
+            continue
+        else:
+            return requested_amount
+
 def GetSub():
     #returns the subreddit instance of the subreddit we will be using
-    
     exists = False
     while exists == False:
         user_req_sub = str(input("Please enter in a subreddit name: "))
         if(user_req_sub == ""):
             continue
         if CheckExists(user_req_sub) == True:
+            print(user_req_sub + " is a valid subreddit")
             return reddit.subreddit(user_req_sub)
         
 def CheckExists(sub_name):
@@ -54,6 +73,8 @@ def CheckForNonImage(sub):
 
 def DownloadImage(sub):
     #downloads and stores image as jpg
+    #returns 1 if succesful
+    #0 if failed
     #TODO: 
         #Create dynamic filepath or create a folder for each time script is ran
         
@@ -61,6 +82,7 @@ def DownloadImage(sub):
         img_content = requests.get(sub.url).content
     except Exception as e:
         print(f'Could not download {sub.url} - {e}')
+        return 0
     #create filename based off url
     file_name = sub.id
     try:
@@ -70,8 +92,10 @@ def DownloadImage(sub):
         with open(file_path, 'wb') as f:
             img.save(f, "JPEG", quality=85)
         print(f"SUCCESS - saved {sub.url} - as {file_path}")
+        return 1
     except Exception as e:
         print(f'Could not download {sub.url} - {e}')
+        return 0
 
 main()
 
