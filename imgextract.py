555491a3212452af1cbc332f7b9d152d610743b0
Bryan Aguilar <b.aguilar4@gmail.com>
2020-02-05 21:09:13 -0800

    Removed ParseTitle,Completed NonImageCheck

--- imgextract.py
+++ imgextract.py
@@ -4,6 +4,8 @@ import io
 from PIL import Image
 import re
 import os
+import pprint
+
 #establish reddit instance
 reddit = praw.Reddit(client_id='oFIVlnHQfgXjYQ',
                      client_secret='xt6o0AWVDm23ytFkZaav4L-sKZc',
@@ -11,7 +13,6 @@ reddit = praw.Reddit(client_id='oFIVlnHQfgXjYQ',
 
 def main():
     #TODO:
-        #scrub the list to find remove non img links
         #check for duplicates
         #allow user to define their own subreddit
         #allow user to define their own search criteria(top/hot/recent/controversial)
@@ -19,61 +20,54 @@ def main():
 
     #stores a list of urls to a list of 10 hottest submissions
     earth_porn_sr = reddit.subreddit('EarthPorn')
-    links = []
-    for submission in earth_porn_sr.top(limit=10):
-        links.append(submission.url)
-    for i in links:
-        print(CheckForNonImage(i))
-def CheckForNonImage(url):
-    #check to see if the passed in URL is a direct image
-    #returns True or False 
-    content = requests.get(url).text
-    title = content[content.find('<title>') + 7 : content.find('</title>')]
-    return title
-
-def ParseTitle(url):
-    #parses the title using regular expressions.
-    #this title will be used as our file name
-    #TODO:
-        #clean up the regex
-        #pretty sure the stripping loop isnt neccessary
-
-    title = re.compile(r'/\w+(?:\.\w{3}$)')
-    
-    #convert list to string so its only single element
-    parsed = parsed[0]
-    final = ''
-    #strips the first slash and file extension
-    for i in range(1,len(parsed)):
-        if parsed[i] == '.':
+    ids = []
+    num_images = 5
+    for submission in earth_porn_sr.top():
+        if len(ids) == num_images:
             break
-        else:
-            final += parsed[i]
-    return final
+        if CheckForNonImage(submission.id) == True:
+            ids.append(submission.id)
+    for i in ids:
+        DownloadImage(i)
 
 
-def DownloadImage(url):
+   
+
+def CheckForNonImage(submission_id):
+    #check .is_reddit_media_domain
+    #and also ensure the .domain == 'i.redd.it'
+    #may  have false flags by allowing videos or gifs through
+
+    post = reddit.submission(id = submission_id)
+    domain = post.domain
+    if domain == 'i.redd.it' or domain == 'i.imgur.com':
+        return True
+    else:
+        #print("Not an image: " + post.title + " " +  post.url + " " + post.domain)
+        return False
+   
+def DownloadImage(submission_id):
     #downloads and stores image as jpg
     #TODO: 
         #Create dynamic filepath or create a folder for each time script is ran
-        #Done - 2/4/2020-BGA Create dynamic filename
-        #
 
+    x = reddit.submission(id=submission_id)
+    url_link = x.url
     try:
-        img_content = requests.get(url).content
+        img_content = requests.get(url_link).content
     except Exception as e:
-        print(f'Could not download {url} - {e}')
-    #create filename based off url
-    file_name = ParseTitle(url)
+        print(f'Could not download {url_link} - {e}')
+    #create filename based off id given
+    file_name = submission_id
     try:
         img_file = io.BytesIO(img_content)
         img = Image.open(img_file).convert('RGB')
-        file_path = os.path.join('C:\\Users\\bagui\\py\\imgextract\\' + file_name + '.jpg')
+        file_path = os.path.join('C:\\Users\\Bryan\\Py\\Save Images Here\\' + file_name + '.jpg')
         with open(file_path, 'wb') as f:
             img.save(f, "JPEG", quality=85)
-        print(f"SUCCESS - saved {url} - as {file_path}")
+        print(f"SUCCESS - saved {submission_id} - as {file_path}")
     except Exception as e:
-        print(f'Could not download {url} - {e}')
+        print(f'Could not download {url_link} - {e}')
 
 main()
 
